1b4b9d3054a748351a62a7001dbf39ff3700654b
Ray Grasso <ray.grasso@gmail.com>
2016-01-22 14:31:12 +0800

    Add example of triggering a sync action from the admin UI

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,32 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+  page_action :sync_events, :method => :post do
+    job_id = SyncEventsWorker.perform_async
+
+    redirect_to admin_dashboard_path, :notice => "Triggered SyncEventsWorker with job id: #{job_id}."
+  end
+
+  content title: proc{ "Dashboard" } do
+    columns do
+      column do
+        panel "What is this?" do
+          para "This is the back end to the Spacecubed mobile application."
+          para "It caches data from Nexudus and serves up a JSON API that the apps consume."
+        end
+      end
+      column do
+        panel "System Actions" do
+          div do
+            link_to "Sync all events",
+              admin_dashboard_sync_events_path,
+              method: :post,
+              class: "button",
+              style: "margin: 5px 0"
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
