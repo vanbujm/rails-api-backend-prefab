325db044e9397c86568f3d62f119501468bd433c
Jonathan Van Buren <jvburen@thoughtworks.com>
2016-10-20 11:38:33 +0800

    Changed initializer for paperclip with s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,5 +5,5 @@ unless Rails.env.development?
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
   # # config/initializers/paperclip.rb
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
 end
